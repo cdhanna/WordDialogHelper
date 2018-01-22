13d713c457dc4364e686ce3d49c657e84c86224e
Christopher Hanna channa@aptima.com <channa@aptima.com>
2018-01-22 16:49:34 -0500

    #adds very basic shell

---
+++ DialogHelper/DialogAddin/DialogRibbon.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using Office = Microsoft.Office.Core;
+using Word = Microsoft.Office.Interop.Word;
+// TODO:  Follow these steps to enable the Ribbon (XML) item:
+
+// 1: Copy the following code block into the ThisAddin, ThisWorkbook, or ThisDocument class.
+
+//  protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
+//  {
+//      return new DialogRibbon();
+//  }
+
+// 2. Create callback methods in the "Ribbon Callbacks" region of this class to handle user
+//    actions, such as clicking a button. Note: if you have exported this Ribbon from the Ribbon designer,
+//    move your code from the event handlers to the callback methods and modify the code to work with the
+//    Ribbon extensibility (RibbonX) programming model.
+
+// 3. Assign attributes to the control tags in the Ribbon XML file to identify the appropriate callback methods in your code.  
+
+// For more information, see the Ribbon XML documentation in the Visual Studio Tools for Office Help.
+
+
+namespace DialogAddin
+{
+    [ComVisible(true)]
+    public class DialogRibbon : Office.IRibbonExtensibility
+    {
+        private Office.IRibbonUI ribbon;
+
+        public DialogRibbon()
+        {
+          
+        }
+
+        #region IRibbonExtensibility Members
+
+        public string GetCustomUI(string ribbonID)
+        {
+            return GetResourceText("DialogAddin.DialogRibbon.xml");
+        }
+
+        #endregion
+
+        #region Ribbon Callbacks
+        //Create callback methods here. For more information about adding callback methods, visit http://go.microsoft.com/fwlink/?LinkID=271226
+
+        public void Ribbon_Load(Office.IRibbonUI ribbonUI)
+        {
+            this.ribbon = ribbonUI;
+            
+        }
+
+        public void OnAddRule(Office.IRibbonControl ribbon)
+        {
+            Word.Range currentRange = Globals.ThisAddIn.Application.Selection.Range;
+            currentRange.Text = "Rule:New\n";
+            currentRange.set_Style(Word.WdBuiltinStyle.wdStyleHeading1);
+
+            var end = currentRange.End ;
+            var nextRange = Globals.ThisAddIn.Application.ActiveDocument.Range(end);
+            nextRange.Text = "\nnext";
+            nextRange.set_Style(Word.WdBuiltinStyle.wdStyleNormal);
+
+        }
+
+        public void OnSave(Office.IRibbonControl ribbon)
+        {
+            var doc = Globals.ThisAddIn.Application.ActiveDocument;
+            var paragraphs = doc.Paragraphs;
+            foreach (Word.Paragraph p in paragraphs)
+            {
+                var text = p.Range.Text;
+                var style = p.Range.get_Style();
+                var isHeader = style.NameLocal == doc.Styles[Word.WdBuiltinStyle.wdStyleHeading1].NameLocal;
+                
+               // var isHeader = p.Range.get_Style() == Word.WdBuiltinStyle.wdStyleHeading1;
+               // var isNormal = p.Range.ParagraphStyle == Word.WdBuiltinStyle.wdStyleNormal;
+            }
+
+            var dialog = new SaveFileDialog();
+            dialog.DefaultExt = ".json";
+            dialog.AddExtension = true;
+            dialog.Filter = "JSON Files|*.json";
+            var result = dialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                var savePath = dialog.FileName;
+            }
+
+        }
+
+        #endregion
+
+        #region Helpers
+
+        private static string GetResourceText(string resourceName)
+        {
+            Assembly asm = Assembly.GetExecutingAssembly();
+            string[] resourceNames = asm.GetManifestResourceNames();
+            for (int i = 0; i < resourceNames.Length; ++i)
+            {
+                if (string.Compare(resourceName, resourceNames[i], StringComparison.OrdinalIgnoreCase) == 0)
+                {
+                    using (StreamReader resourceReader = new StreamReader(asm.GetManifestResourceStream(resourceNames[i])))
+                    {
+                        if (resourceReader != null)
+                        {
+                            return resourceReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        #endregion
+    }
+}
---
+++ DialogHelper/DialogAddin/ThisAddIn.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Word = Microsoft.Office.Interop.Word;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Word;
+
+namespace DialogAddin
+{
+    public partial class ThisAddIn
+    {
+        
+
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            this.Application.DocumentChange += Application_DocumentChange;
+            Application.DocumentOpen += Application_DocumentOpen;
+
+
+        }
+
+        private void Application_DocumentOpen(Word.Document Doc)
+        {
+            
+        }
+
+        private void Application_DocumentChange()
+        {
+            var doc = this.Application.ActiveDocument;
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+        protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
+        {
+            return new DialogRibbon();
+        }
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
